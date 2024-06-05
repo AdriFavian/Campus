d34c559a6fdeb00d9fd25fa3c7d4f8768ec1569f
AdriFavian <adrifavian2@gmail.com>
2024-06-05 15:06:58 +0700

    update

---
+++ Semester-2/Algoritma dan Struktur Data/week-12/src/tugas_praktikum/no_1/DoubleLinkedList.java
@@ -0,0 +1,56 @@
+public class DoubleLinkedList {
+    Node head, tail;
+    int size;
+
+    public DoubleLinkedList() {
+        head = tail = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addLast(int nomorAntrian, String nama) {
+        if (isEmpty()) {
+            head = tail = new Node(null, nomorAntrian, nama, null);
+        } else {
+            Node newNode = new Node(tail, nomorAntrian, nama, null);
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Tidak ada pengantri yang bisa dihapus!");
+        } else {
+            System.out.println(head.nama + " telah selesai divaksinasi.");
+            if (head.next == null) {
+                head = tail = null;
+            } else {
+                head = head.next;
+                head.prev = null;
+            }
+            size--;
+        }
+    }
+
+    public void printQueue() {
+        if (isEmpty()) {
+            System.out.println("Tidak ada pengantri.");
+        } else {
+            Node current = head;
+            System.out.println("+++++++++++++++++++++++");
+            System.out.println("Daftar Pengantri Vaksin");
+            System.out.println("+++++++++++++++++++++++");
+            System.out.println("|No.   |Nama        |");
+            while (current != null) {
+                System.out.println("|" + current.nomorAntrian + "      |" + current.nama + "            |");
+                current = current.next;
+            }
+            System.out.println("Sisa Antrian: " + size);
+        }
+    }
+}
---
+++ Semester-2/Algoritma dan Struktur Data/week-12/src/tugas_praktikum/no_1/VaksinasiMain.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class VaksinasiMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DoubleLinkedList queue = new DoubleLinkedList();
+        int choice;
+
+        do {
+            System.out.println("+++++++++++++++++++++++++++++");
+            System.out.println("PENGANTRI VAKSIN EXTRAVAGANZA");
+            System.out.println("+++++++++++++++++++++++++++++");
+            System.out.println("1. Tambah Data Penerima Vaksin");
+            System.out.println("2. Hapus Data Pengantri Vaksin");
+            System.out.println("3. Daftar Penerima Vaksin");
+            System.out.println("4. Keluar");
+            System.out.println("+++++++++++++++++++++++++++++");
+            choice = scanner.nextInt();
+            scanner.nextLine(); 
+
+            switch (choice) {
+                case 1:
+                    System.out.println("-----------------------------");
+                    System.out.println("Masukkan Data Penerima Vaksin");
+                    System.out.println("-----------------------------");
+                    System.out.print("Nomor Antrian: ");
+                    int nomorAntrian = scanner.nextInt();
+                    scanner.nextLine();  
+                    System.out.print("Nama Penerima: ");
+                    String nama = scanner.nextLine();
+                    queue.addLast(nomorAntrian, nama);
+                    break;
+                case 2:
+                    if (!queue.isEmpty()) {
+                        queue.removeFirst();
+                    } else {
+                        System.out.println("Tidak ada pengantri yang bisa dihapus!");
+                    }
+                    break;
+                case 3:
+                    queue.printQueue();
+                    break;
+                case 4:
+                    System.out.println("Terima kasih telah menggunakan layanan ini!");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid, silakan pilih lagi.");
+            }
+        } while (choice != 4);
+
+        scanner.close();
+    }
+}
